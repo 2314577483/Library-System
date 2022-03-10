225f3d187f5e88df89f24a52123b293527cdffa0
WwwwwFeng <97133827+WwwwwFeng@users.noreply.github.com>
2022-03-09 23:13:29 -0800

    Update unit_tests.cpp

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -2,6 +2,13 @@
 #include "NewuserTests.hpp"
 #include "StudentTests.hpp"
 #include "LibrarianTests.hpp"
+#include "ItemTests.hpp"
+#include "ShelfTests.hpp"
+#include "BookTests.hpp"
+#include "ArticleTests.hpp"
+#include "VideoTests.hpp"
+#include "SearchTests.hpp"
+
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
